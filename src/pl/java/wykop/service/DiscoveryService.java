5bc098206ae4716293fe72d260843e3b089f23a7
MarekBorsuk <markus19910222@gmail.com>
2018-08-13 21:31:47 +0200

    Add discovery

--- src/pl/java/wykop/service/DiscoveryService.java
+++ src/pl/java/wykop/service/DiscoveryService.java
@@ -1,5 +1,29 @@
 package pl.java.wykop.service;
 
-public class DiscoveryService {
+import java.sql.Timestamp;
+import java.util.Date;
 
+import pl.java.wykop.dao.DAOFactory;
+import pl.java.wykop.dao.DiscoveryDAO;
+import pl.java.wykop.model.Discovery;
+import pl.java.wykop.model.User;
+
+public class DiscoveryService {
+	public void addDiscovery(String name, String desc, String url, User user) {
+		Discovery discovery = createDiscoveryObject(name, desc, url, user);
+		DAOFactory factory = DAOFactory.getDAOFactory();
+		DiscoveryDAO discoveryDao = factory.getDiscoveryDAO();
+		discoveryDao.create(discovery);
+	}
+	
+	private Discovery createDiscoveryObject(String name, String desc, String url, User user) {
+		Discovery discovery = new Discovery();
+		discovery.setName(name);
+		discovery.setDescription(desc);
+		discovery.setUrl(url);
+		User userCopy = new User(user);
+		discovery.setUser(userCopy);
+		discovery.setTimestamp(new Timestamp(new Date().getTime()));
+		return discovery;
+	}
 }
