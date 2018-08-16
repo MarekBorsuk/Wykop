94412729dd5de89e6c6ee6a24d02628cd77a3e5a
MarekBorsuk <markus19910222@gmail.com>
2018-08-16 18:48:11 +0200

    add home page with in all discoveries

--- src/pl/java/wykop/dao/DiscoveryDAOImpl.java
+++ src/pl/java/wykop/dao/DiscoveryDAOImpl.java
@@ -1,9 +1,12 @@
 package pl.java.wykop.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -11,13 +14,18 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
 import pl.java.wykop.model.Discovery;
+import pl.java.wykop.model.User;
 import pl.java.wykop.util.ConnectionProvider;
 
-public class DiscoveryDAOImpl implements DiscoveryDAO{
+public class DiscoveryDAOImpl implements DiscoveryDAO{	
+
 	private static final String CREATE_DISCOVERY =
 			  "INSERT INTO discovery(name, description, url, user_id, date, up_vote, down_vote) "
 					  + "VALUES(:name, :description, :url, :user_id, :date, :up_vote, :down_vote);";
-
+	private static final String READ_ALL_DISCOVERIES = 
+			"SELECT user.user_id, username, email, is_active, password, discovery_id, name, description, url, date, up_vote, down_vote "
+					  + "FROM discovery LEFT JOIN user ON discovery.user_id=user.user_id;";
+	
 	private NamedParameterJdbcTemplate template;
 	
 	public DiscoveryDAOImpl() {
@@ -64,8 +72,30 @@ public class DiscoveryDAOImpl implements DiscoveryDAO{
 
 	@Override
 	public List<Discovery> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Discovery> discoveries = template.query(READ_ALL_DISCOVERIES, new DiscoveryRowMapper());
+		return discoveries;
+	}
+	
+	public class DiscoveryRowMapper implements RowMapper<Discovery> {
+		@Override
+		public Discovery mapRow(ResultSet resultSet, int row) throws SQLException {
+			Discovery discovery = new Discovery();
+			discovery.setId(resultSet.getLong("discovery_id"));
+			discovery.setName(resultSet.getString("name"));
+			discovery.setDescription(resultSet.getString("description"));
+			discovery.setUrl(resultSet.getString("url"));
+			discovery.setUpVote(resultSet.getInt("up_vote"));
+			discovery.setDownVote(resultSet.getInt("down_vote"));
+			discovery.setTimestamp(resultSet.getTimestamp("date"));
+			User user = new User();
+			user.setId(resultSet.getLong("user_id"));
+			user.setUsername(resultSet.getString("username"));
+			user.setEmail(resultSet.getString("email"));
+			user.setPassword(resultSet.getString("password"));
+			discovery.setUser(user);
+			return discovery;
+		}
+
 	}
 
 }
